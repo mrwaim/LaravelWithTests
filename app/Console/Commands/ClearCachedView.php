9db15c00a0b5b722d3e6d066f193ef7d053b02d7
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-05-10 18:46:26 +0800

    clear cached views

---
+++ app/Console/Commands/ClearCachedView.php
@@ -0,0 +1,38 @@
+<?php namespace App\Console\Commands;
+
+use Config;
+use Illuminate\Console\Command;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Filesystem\Filesystem;
+
+// From https://gist.github.com/lucasmichot/7313220
+class ClearCachedView extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'cache:clearblade';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Flush the application Blade engine cache.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire() {
+        $fileSystem = new Filesystem();
+
+        $fileSystem->remove(Finder::create()->in(Config::get('view.compiled'))->files());
+
+        $this->comment('Cleard compiled views');
+    }
+
+}
