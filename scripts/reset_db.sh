c04355ef6d5d6a9f3c5a3c1f4211873af9f8be39
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-05-10 16:10:11 +0800

    add reset db script

---
+++ scripts/reset_db.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+if [ "$1" == "" ]; then
+    echo "usage reset_db.sh database_name"
+    exit 1;
+fi
+
+mysql -u root -e "drop database $1; create database $1;"
+php artisan migrate
+php artisan -v db:seed
+
