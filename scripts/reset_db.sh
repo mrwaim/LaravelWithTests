1ed9871d7c75479e08d83f1f88b9dbbe0462f98c
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-05-10 17:18:53 +0800

    reset db takes username

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -4,7 +4,24 @@ if [ "$1" == "" ]; then
     exit 1;
 fi
 
+user="root"
+p=""
+
+if [ -z "$2" ]; then
+	echo "using $user"
+else
+	user=$2
+	p=" -p "
+	echo "using $user"
+fi
+
 composer dump-autoload
-mysql -u root -e "drop database $1; create database $1;"
+command="mysql -u $user $p -e \"drop database $1; create database $1;\""
+echo $command
+if ! eval $command; then
+    echo "mysql failed" >&2
+    exit 1
+fi
+
 php artisan migrate
 php artisan -v db:seed
