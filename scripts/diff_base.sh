647a22858f8ba0eb1b09862515ca9807f19a5cab
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-05-23 14:18:03 +0800

    update diff base

--- scripts/diff_base.sh
+++ scripts/diff_base.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 echo $1
-diff -r --brief . $1 | grep differ | grep -v './vendor\|./resources\|./nbproject\|./database/migrations\|\./\.\|./_ide_helper\.php\|./app/Http/Controllers\|./database/seeds\|./public\|./tests/_output\|./tests/acceptance\|User.php\|Registrar.php'
+pattern="./vendor\|./resources\|./nbproject\|./database/migrations\|\./\.\|$1/\.\|./_ide_helper\.php\|./app/Http/Controllers\|./database/seeds\|./public\|./tests/_output\|./tests/acceptance\|User.php\|Registrar.php\|./storage/"
+mytemp=$(mktemp -t diff)
+#echo $mytemp
+diff -r --brief . $1 > $mytemp
+cat $mytemp | grep differ | grep -v './vendor\|./resources\|./nbproject\|./database/migrations\|\./\.\|./_ide_helper\.php\|./app/Http/Controllers\|./database/seeds\|./public\|./tests/_output\|./tests/acceptance\|User.php\|Registrar.php'
+cat $mytemp | grep 'Only in' | grep $1 | grep -v $pattern
