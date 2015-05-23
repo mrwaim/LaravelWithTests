72703d193722a0c84101cabb3105aa186cf70466
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-05-23 20:18:58 +0800

    show only in extra files

--- scripts/diff_base.sh
+++ scripts/diff_base.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 echo $1
-pattern="./vendor\|./resources\|./nbproject\|./database/migrations\|\./\.\|$1/\.\|./_ide_helper\.php\|./app/Http/Controllers\|./database/seeds\|./public\|./tests/_output\|./tests/acceptance[:/]\|User.php\|Registrar.php\|./storage/"
+pattern="./vendor\|./resources\|./nbproject\|./database/migrations\|\./\.\|$1/\.\|./_ide_helper\.php\|./app/Http/Controllers\|./database/seeds\|./public\|./tests/_output\|./tests/acceptance[:/]\|User.php\|Registrar.php\|./storage/\|\.git\|\.DS_Store"
 mytemp=$(mktemp -t diff)
 #echo $mytemp
 diff -r --brief . $1 > $mytemp
+#cat $mytemp  | grep -v $pattern
 cat $mytemp | grep differ | grep -v $pattern
 cat $mytemp | grep 'Only in' | grep $1 | grep -v $pattern
+cat $mytemp | grep 'Only in' | grep -v $1 | grep -v $pattern
