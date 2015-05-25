9f0a2f133b94dd07e5f99ccad2705774ca370803
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-05-25 12:41:32 +0800

    refactor list models

---
+++ app/Console/Commands/EditMoveModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use File;
+use Symfony\Component\Finder\SplFileInfo;
+
+class ListModel extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'list:model';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List all models.';
+
+    public static function getAllModels()
+    {
+        $files = File::allFiles(app_path());
+        foreach ($files as $file) {
+            /* @var $file SplFileInfo */
+            $noExt = preg_replace("/\.[^.]+$/", "", $file->getBasename());
+            $f = $file->openFile();
+            $content = $f->fread($f->getSize());
+
+            if (str_contains($content, "class $noExt extends Model")) {
+                yield $file->getFilename();
+            }
+        }
+    }
+    
+    public function fire() {
+        foreach (self::getAllModels() as $model) {
+            $this->comment($model);
+        }
+    }
+
+}
